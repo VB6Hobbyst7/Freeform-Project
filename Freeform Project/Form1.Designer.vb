0d401ef825146d783836720424d5d584321a060b
nicknewton100 <nicholasnewton27@yahoo.com>
2018-08-29 14:37:46 -0400

    kms

--- Freeform Project/Form1.Designer.vb
+++ Freeform Project/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,66 +20,32 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Rbtn = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.RDMbox = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'Rbtn
         '
-        Me.Rbtn.Location = New System.Drawing.Point(219, 121)
+        Me.Rbtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Rbtn.Location = New System.Drawing.Point(176, 219)
         Me.Rbtn.Name = "Rbtn"
-        Me.Rbtn.Size = New System.Drawing.Size(75, 23)
+        Me.Rbtn.Size = New System.Drawing.Size(153, 60)
         Me.Rbtn.TabIndex = 0
         Me.Rbtn.Text = "Randomize"
         Me.Rbtn.UseVisualStyleBackColor = True
-        '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(52, 460)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 1
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(69, 433)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
-        Me.Label1.TabIndex = 2
-        Me.Label1.Text = "Label1"
-        '
-        'RDMbox
-        '
-        Me.RDMbox.Location = New System.Drawing.Point(217, 12)
-        Me.RDMbox.Name = "RDMbox"
-        Me.RDMbox.Size = New System.Drawing.Size(77, 20)
-        Me.RDMbox.TabIndex = 3
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(529, 507)
-        Me.Controls.Add(Me.RDMbox)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Rbtn)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Rbtn As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Label1 As Label
-    Friend WithEvents RDMbox As TextBox
 End Class
--- Freeform Project/Form1.vb
+++ Freeform Project/Form1.vb
@@ -1,45 +1,62 @@
 ﻿Public Class Form1
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
 
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
 
     End Sub
 
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles RDMbox.TextChanged
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
 
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Rbtn.Click
-        Call Randomize()
-    End Sub
-    Sub Randomize()
+        Dim number As Integer
 
-        Dim rgn As New Random()
-        Dim RAND(6) As String
-        RAND(0) = "0"
-        RAND(1) = "1"
-        RAND(2) = "2"
-        RAND(3) = "3"
-        RAND(4) = "4"
-        RAND(5) = "5"
-        RAND(6) = "6"
-        RAND(7) = "7"
-        RAND(8) = "8"
-        RAND(9) = "9"
-        RAND(10) = "10"
-        RAND(11) = "11"
-        RAND(12) = "12"
-        RAND(13) = "13"
-        RAND(14) = "14"
-        RAND(15) = "15"
-        RAND(16) = "16"
-        RAND(17) = "17"
-        RAND(18) = "18"
-        RAND(19) = "19"
-        RAND(20) = "20"
+        Randomize()
+        number = Int(Rnd() * 6) + 1
 
-        RDMbox.Text = rgn.Next(RAND.Count())
+        MessageBox.Show(number)
+
+        ' If (number = 1) Then
+        'MessageBox.Show("You rolled 1")
+        'End If
+
+        '  If (number > 31 And number < 40) Then
+        ' MessageBox.Show("IDK")
+        ' End If
+        'Call Randomize()
+
+
+
     End Sub
+    ' Sub Randomize()
+
+    ' Dim rgn As New Random()
+    ' Dim RAND(6) As String
+    'RAND(0) = "0"
+    '  RAND(1) = "1"
+    '  RAND(2) = "2"
+    '  RAND(3) = "3"
+    '  RAND(4) = "4"
+    ' RAND(5) = "5"
+    '  RAND(6) = "6"
+    '  RAND(7) = "7"
+    ' RAND(8) = "8"
+    '  RAND(9) = "9"
+    '' RAND(10) = "10"
+    ' RAND(11) = "11"
+    ' RAND(12) = "12"
+    ' RAND(13) = "13"
+    ' RAND(14) = "14"
+    ' RAND(15) = "15"
+    ' RAND(16) = "16"
+    ' RAND(17) = "17"
+    ' RAND(18) = "18"
+    '  RAND(19) = "19"
+    ' RAND(20) = "20"
+
+    'RDMbox.Text = rgn.Next(RAND.Count())
+    'End Sub
 End Class
