8194c61204b4696e8ca73fa2d3e9622048a1a2ff
nicknewton100 <nicholasnewton27@yahoo.com>
2018-09-03 21:50:42 -0400

    YAY

--- Freeform Project/Form1.Designer.vb
+++ Freeform Project/Form1.Designer.vb
@@ -24,40 +24,38 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Rbtn = New System.Windows.Forms.Button()
         Me.selectbox = New System.Windows.Forms.ComboBox()
-        Me.Label1 = New System.Windows.Forms.Label()
         Me.resbtn = New System.Windows.Forms.Button()
-        Me.tb2 = New System.Windows.Forms.TextBox()
-        Me.tb4 = New System.Windows.Forms.TextBox()
-        Me.tb3 = New System.Windows.Forms.TextBox()
-        Me.tb1 = New System.Windows.Forms.TextBox()
+        Me.Box2 = New System.Windows.Forms.TextBox()
+        Me.Box4 = New System.Windows.Forms.TextBox()
+        Me.Box3 = New System.Windows.Forms.TextBox()
+        Me.Box1 = New System.Windows.Forms.TextBox()
+        Me.NBox1 = New System.Windows.Forms.TextBox()
+        Me.NBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Min = New System.Windows.Forms.Label()
+        Me.Max = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Rbtn
         '
+        Me.Rbtn.Enabled = False
         Me.Rbtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Rbtn.Location = New System.Drawing.Point(192, 177)
+        Me.Rbtn.Location = New System.Drawing.Point(194, 175)
         Me.Rbtn.Name = "Rbtn"
-        Me.Rbtn.Size = New System.Drawing.Size(153, 60)
+        Me.Rbtn.Size = New System.Drawing.Size(151, 60)
         Me.Rbtn.TabIndex = 0
         Me.Rbtn.Text = "Randomize"
         Me.Rbtn.UseVisualStyleBackColor = True
         '
         'selectbox
         '
+        Me.selectbox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.selectbox.FormattingEnabled = True
-        Me.selectbox.Location = New System.Drawing.Point(207, 85)
+        Me.selectbox.Items.AddRange(New Object() {"Numbers", "Input"})
+        Me.selectbox.Location = New System.Drawing.Point(208, 123)
         Me.selectbox.Name = "selectbox"
         Me.selectbox.Size = New System.Drawing.Size(121, 21)
         Me.selectbox.TabIndex = 1
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(182, 52)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(163, 13)
-        Me.Label1.TabIndex = 2
-        Me.Label1.Text = "What do you want to randomize?"
         '
         'resbtn
         '
@@ -68,47 +66,100 @@ Partial Class Form1
         Me.resbtn.Text = "Reset"
         Me.resbtn.UseVisualStyleBackColor = True
         '
-        'tb2
+        'Box2
         '
-        Me.tb2.Location = New System.Drawing.Point(88, 416)
-        Me.tb2.Name = "tb2"
-        Me.tb2.Size = New System.Drawing.Size(100, 20)
-        Me.tb2.TabIndex = 4
+        Me.Box2.Location = New System.Drawing.Point(88, 416)
+        Me.Box2.Name = "Box2"
+        Me.Box2.Size = New System.Drawing.Size(100, 20)
+        Me.Box2.TabIndex = 4
+        Me.Box2.Visible = False
         '
-        'tb4
+        'Box4
         '
-        Me.tb4.Location = New System.Drawing.Point(352, 416)
-        Me.tb4.Name = "tb4"
-        Me.tb4.Size = New System.Drawing.Size(100, 20)
-        Me.tb4.TabIndex = 5
+        Me.Box4.Location = New System.Drawing.Point(352, 416)
+        Me.Box4.Name = "Box4"
+        Me.Box4.Size = New System.Drawing.Size(100, 20)
+        Me.Box4.TabIndex = 5
+        Me.Box4.Visible = False
         '
-        'tb3
+        'Box3
         '
-        Me.tb3.Location = New System.Drawing.Point(352, 303)
-        Me.tb3.Name = "tb3"
-        Me.tb3.Size = New System.Drawing.Size(100, 20)
-        Me.tb3.TabIndex = 6
+        Me.Box3.Location = New System.Drawing.Point(352, 303)
+        Me.Box3.Name = "Box3"
+        Me.Box3.Size = New System.Drawing.Size(100, 20)
+        Me.Box3.TabIndex = 6
+        Me.Box3.Visible = False
         '
-        'tb1
+        'Box1
         '
-        Me.tb1.Location = New System.Drawing.Point(88, 303)
-        Me.tb1.Name = "tb1"
-        Me.tb1.Size = New System.Drawing.Size(100, 20)
-        Me.tb1.TabIndex = 7
+        Me.Box1.Location = New System.Drawing.Point(88, 303)
+        Me.Box1.Name = "Box1"
+        Me.Box1.Size = New System.Drawing.Size(100, 20)
+        Me.Box1.TabIndex = 7
+        Me.Box1.Visible = False
+        '
+        'NBox1
+        '
+        Me.NBox1.Location = New System.Drawing.Point(115, 360)
+        Me.NBox1.Name = "NBox1"
+        Me.NBox1.Size = New System.Drawing.Size(100, 20)
+        Me.NBox1.TabIndex = 8
+        Me.NBox1.Visible = False
+        '
+        'NBox2
+        '
+        Me.NBox2.Location = New System.Drawing.Point(324, 360)
+        Me.NBox2.Name = "NBox2"
+        Me.NBox2.Size = New System.Drawing.Size(100, 20)
+        Me.NBox2.TabIndex = 9
+        Me.NBox2.Visible = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(191, 97)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(163, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "What do you want to randomize?"
+        '
+        'Min
+        '
+        Me.Min.AutoSize = True
+        Me.Min.Location = New System.Drawing.Point(140, 344)
+        Me.Min.Name = "Min"
+        Me.Min.Size = New System.Drawing.Size(48, 13)
+        Me.Min.TabIndex = 10
+        Me.Min.Text = "Minimum"
+        Me.Min.Visible = False
+        '
+        'Max
+        '
+        Me.Max.AutoSize = True
+        Me.Max.Location = New System.Drawing.Point(349, 344)
+        Me.Max.Name = "Max"
+        Me.Max.Size = New System.Drawing.Size(51, 13)
+        Me.Max.TabIndex = 11
+        Me.Max.Text = "Maximum"
+        Me.Max.Visible = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(529, 507)
-        Me.Controls.Add(Me.tb1)
-        Me.Controls.Add(Me.tb3)
-        Me.Controls.Add(Me.tb4)
-        Me.Controls.Add(Me.tb2)
-        Me.Controls.Add(Me.resbtn)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.selectbox)
+        Me.Controls.Add(Me.Max)
+        Me.Controls.Add(Me.Min)
         Me.Controls.Add(Me.Rbtn)
+        Me.Controls.Add(Me.NBox2)
+        Me.Controls.Add(Me.NBox1)
+        Me.Controls.Add(Me.Box1)
+        Me.Controls.Add(Me.Box3)
+        Me.Controls.Add(Me.Box4)
+        Me.Controls.Add(Me.Box2)
+        Me.Controls.Add(Me.resbtn)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.selectbox)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -118,10 +169,14 @@ Partial Class Form1
 
     Friend WithEvents Rbtn As Button
     Friend WithEvents selectbox As ComboBox
-    Friend WithEvents Label1 As Label
     Friend WithEvents resbtn As Button
-    Friend WithEvents tb2 As TextBox
-    Friend WithEvents tb4 As TextBox
-    Friend WithEvents tb3 As TextBox
-    Friend WithEvents tb1 As TextBox
+    Friend WithEvents Box2 As TextBox
+    Friend WithEvents Box4 As TextBox
+    Friend WithEvents Box3 As TextBox
+    Friend WithEvents Box1 As TextBox
+    Friend WithEvents NBox1 As TextBox
+    Friend WithEvents NBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Min As Label
+    Friend WithEvents Max As Label
 End Class
--- Freeform Project/Form1.vb
+++ Freeform Project/Form1.vb
@@ -13,18 +13,25 @@
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Rbtn.Click
         Dim number As Integer
+        If selectbox.SelectedItem = "Numbers" Then
+            Randomize()
+            number = Int((NBox1.Text - NBox2.Text * -1) * Rnd()) + 1
 
-        Randomize()
-        number = Int(Rnd() * 72) + 1
+            MessageBox.Show(number)
 
-        MessageBox.Show(number)
+        ElseIf selectbox.SelectedItem = "Input" Then
+            Randomize()
 
-        ' If (number = 1) Then
-        'MessageBox.Show("You rolled 1")
-        'End If
+            Dim rgn As New Random()
+            Dim RAND(3) As String
 
-        If (number > 31 And number < 40) Then
-            MessageBox.Show("You get on my nerves")
+            RAND(0) = Box1.Text
+            RAND(1) = Box2.Text
+            RAND(2) = Box3.Text
+            RAND(3) = Box4.Text
+
+            MessageBox.Show(RAND(Rnd))
+
         End If
         Call Randomize()
 
@@ -33,42 +40,44 @@
     End Sub
 
     Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles resbtn.Click
-
+        Box1.Clear()
+        Box2.Clear()
+        Box3.Clear()
+        Box4.Clear()
+        NBox1.Clear()
+        NBox2.Clear()
     End Sub
 
     Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles selectbox.SelectedIndexChanged
+        If selectbox.SelectedItem = "Input" Then
+            Box1.Visible = True
+            Box2.Visible = True
+            Box3.Visible = True
+            Box4.Visible = True
+            NBox1.Visible = False
+            NBox2.Visible = False
+            Min.Visible = False
+            Max.Visible = False
+            Rbtn.Enabled = True
+        ElseIf selectbox.SelectedItem = "Numbers" Then
+            NBox1.Visible = True
+            NBox2.Visible = True
+            Box1.Visible = False
+            Box2.Visible = False
+            Box3.Visible = False
+            Box4.Visible = False
+            Min.Visible = True
+            Max.Visible = True
+            Rbtn.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Label1_Click_1(sender As Object, e As EventArgs) Handles Label2.Click
 
     End Sub
 
-    Private Sub Label1_Click_1(sender As Object, e As EventArgs) Handles Label1.Click
+
+    Private Sub TextBox1_TextChanged_1(sender As Object, e As EventArgs)
 
     End Sub
-    ' Sub Randomize()
-
-    ' Dim rgn As New Random()
-    ' Dim RAND(6) As String
-    'RAND(0) = "0"
-    '  RAND(1) = "1"
-    '  RAND(2) = "2"
-    '  RAND(3) = "3"
-    '  RAND(4) = "4"
-    ' RAND(5) = "5"
-    '  RAND(6) = "6"
-    '  RAND(7) = "7"
-    ' RAND(8) = "8"
-    '  RAND(9) = "9"
-    '' RAND(10) = "10"
-    ' RAND(11) = "11"
-    ' RAND(12) = "12"
-    ' RAND(13) = "13"
-    ' RAND(14) = "14"
-    ' RAND(15) = "15"
-    ' RAND(16) = "16"
-    ' RAND(17) = "17"
-    ' RAND(18) = "18"
-    '  RAND(19) = "19"
-    ' RAND(20) = "20"
-
-    'RDMbox.Text = rgn.Next(RAND.Count())
-    'End Sub
 End Class
